abb20e5b81b7025703dca09224a9b231965b0d21
Joey <joelnl74@gmail.com>
2020-02-09 20:54:42 +0100

    Read chessboard corners code.

--- Computer vision/main.cpp
+++ Computer vision/main.cpp
@@ -4,6 +4,10 @@
 int main()
 {
 	cv::Mat image = cv::imread("jpg-vs-jpeg.jpg");
+	cv::FileStorage fs("path here", cv::FileStorage::READ);
+	cv::Mat intrinsics, distortion;
+	fs["camera_matrix"] >> intrinsics;
+	fs["distortion_coefficients"] >> distortion;
 
     if (image.empty())                      
     {
@@ -14,6 +18,10 @@ int main()
 		cv::rectangle(image, cv::Rect(0, 0, 100, 100), cv::Scalar(0, 255, 128));
 		imshow("Display Window", image);
 	}
+	std::vector<cv::Point3f> boardPoints;
+	bool found = cv::findChessboardCorners(image, cv::Size(8, 9), boardPoints, cv::CALIB_CB_ADAPTIVE_THRESH);
+
+
 
 	cv::waitKey(0);
 
