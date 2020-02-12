ea0edb34dbe191151d33a4a937550199953bed97
Joey <joelnl74@gmail.com>
2020-02-12 12:02:59 +0100

    Comments

--- Computer vision/OfflineOpenCV.cpp
+++ Computer vision/OfflineOpenCV.cpp
@@ -4,17 +4,27 @@ void OfflineOpenCV::GetChessBoardCorners(const std::string& imagePath, const int
 {
 	for (int i = 0; i < amount; i++)
 	{
+		// Create image path
 		std::string path = imagePath;
 		path.append(std::to_string(i));
 		path.append(".jpg");
+
+		// Read in images
 		cv::Mat img = cv::imread(path);
 		std::vector<cv::Point2f> corners;
+
+		// Find chess board corners(each cell on the checkerboard).
 		bool succes = cv::findChessboardCorners(img, boardSize, corners);
 
 		if (succes)
 		{
+			// Save the points found on the checkerboard.
 			m_objectPoints.push_back(corners);
+
+			// Draw the found corners on the image.
 			cv::drawChessboardCorners(img, boardSize, corners, succes);
+
+			// Show the image on the scsreen.
 			cv::imshow("img", img);
 
 			cv::waitKey(500);
@@ -26,6 +36,7 @@ void OfflineOpenCV::GetChessBoardCorners(const std::string& imagePath, const int
 
 void OfflineOpenCV::GetWorldSpaceCoords()
 {
+	// Get the known positions on the images and save those.
 	for (int i = 0; i < boardSize.x; i++)
 	{
 		for (int j = 0; j < boardSize.y; j++)
@@ -41,5 +52,5 @@ void OfflineOpenCV::CalibrateCamera()
 	GetChessBoardCorners("images/", 5);
 	GetWorldSpaceCoords();
 
-	cv::calibrateCamera(m_objectPoints, m_imagePoints, cv::Point2f(0.025, 0.025), m_cameraMatrix, distcoefs, rvecs, tvecs);
+	cv::calibrateCamera(m_imagePoints, m_objectPoints, cv::Point2f(0.025, 0.025), m_cameraMatrix, distcoefs, rvecs, tvecs);
 }
